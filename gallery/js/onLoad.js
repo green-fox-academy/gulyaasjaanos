5d26b7b6f1b52f3ccd5765814b830e40568fcba8
gulyaasjaanos <53534194+gulyaasjaanos@users.noreply.github.com>
2020-03-14 13:19:49 +0100

    css layout and pictures added

---
+++ gallery/js/onLoad.js
@@ -0,0 +1,10 @@
+let imgbar = document.getElementsByClassName("imgbar")[0];
+        let imgList = [];
+        const seed = 860;
+        for (let i = 0; i <= 7; i++) {
+            imgList.push(document.createElement("img"));
+            imgList[imgList.length-1].src = `https://picsum.photos/id/${seed+i}/1600/900`
+            imgList[imgList.length-1].alt = `pic#${seed+i}`
+            imgList[imgList.length-1].setAttribute("data-pic", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.");
+        }
+imgbar.append(...imgList);

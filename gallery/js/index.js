fea4fd1771a71e8c90de100031e91d041d8baf02
gulyaasjaanos <53534194+gulyaasjaanos@users.noreply.github.com>
2020-03-14 14:42:36 +0100

    basic functionality

---
+++ gallery/js/index.js
@@ -0,0 +1,36 @@
+let imageIndex = 0;
+
+function fillImage(index) {
+    mainimg.src = imageBarElements[index].src;
+    mainimg.alt = imageBarElements[index].alt;
+    let h3 = document.querySelector(".mainimg header h3");
+    h3.innerHTML = imageBarElements[index].alt;
+    let p = document.querySelector(".mainimg header p");
+    p.innerHTML = imageBarElements[index].getAttribute("data-pic");
+}
+
+const imageBarElements = document.querySelectorAll(".imgbar img");
+const maxIndex = imageBarElements.length - 1;
+fillImage(imageIndex);
+
+imageBarElements.forEach(e => {
+    e.addEventListener("click", f => {
+        imageIndex = e.getAttribute("data-num");
+        fillImage(imageIndex);
+    });
+});
+
+
+const navElementLeft = document.querySelector(".left");
+navElementLeft.addEventListener("click", f => {
+    imageIndex--;
+    imageIndex = (imageIndex < 0)? maxIndex : imageIndex;
+    fillImage(imageIndex);
+});
+
+const navElementRight = document.querySelector(".right");
+navElementRight.addEventListener("click", f => {
+    imageIndex++;
+    imageIndex = (imageIndex > maxIndex)? 0 : imageIndex;
+    fillImage(imageIndex);
+});
--- gallery/js/onLoad.js
+++ gallery/js/onLoad.js
@@ -1,10 +1,13 @@
 let imgbar = document.getElementsByClassName("imgbar")[0];
         let imgList = [];
-        const seed = 860;
-        for (let i = 0; i <= 7; i++) {
+        const seed = Math.floor(Math.random() * 1000) + 1;
+        for (let i = 0; i <= 12; i++) {
             imgList.push(document.createElement("img"));
             imgList[imgList.length-1].src = `https://picsum.photos/id/${seed+i}/1600/900`
             imgList[imgList.length-1].alt = `pic#${seed+i}`
-            imgList[imgList.length-1].setAttribute("data-pic", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.");
+            imgList[imgList.length-1].setAttribute("data-pic", `pic#${seed+i}`+"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.");
+            imgList[imgList.length-1].setAttribute("data-num", i);
         }
 imgbar.append(...imgList);
+
+

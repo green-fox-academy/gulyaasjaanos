17388379f278da04e71f5145535762fca93e5303
gulyaasjaanos <53534194+gulyaasjaanos@users.noreply.github.com>
2020-03-28 17:04:44 +0100

    sith translator added to app

--- rest/postman/app.js
+++ rest/postman/app.js
@@ -38,6 +38,18 @@ app.post("/arrays/", (request, response) => {
 });
 
 
+app.post("/sith/", (request, response) => {
+    let output = {
+        "error": "Feed me some text you have to, padawan young you are. Hmmm."
+    }
+    if (request.body.text) {
+        output = {
+            "sith_text": sithText(request.body.text)
+        }
+    }
+    response.send(output);
+});
+
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 });
@@ -58,4 +70,40 @@ function arraysMultiply(numbers) {
 
 function arraysDouble(numbers) {
     return numbers.map( (e) => e * 2);
+};
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const randomYoda = () => {
+    const cleverness = [
+        "Hmmm.",
+        "Err..err.err.",
+        "Arrrgh.",
+        "Uhhm.",
+        "Good."
+    ]
+    let d = Math.floor(Math.random() * 2) + 1;
+    let n1 = Math.floor(Math.random() * cleverness.length);
+    let n2 = Math.floor(Math.random() * cleverness.length);
+    return (d === 1)? cleverness[n1] : cleverness[n1] + " " + cleverness[n2];
+};
+    
+
+
+
+function sithText(text) {
+    let textArray = text.split(". ");
+    let newArray = textArray.map( (e) => {
+        let eArray = e.split(" ");
+        for (let i = 0; i < eArray.length-1; i = i + 2) {
+            let A = (i === 0)? eArray[i].toLowerCase() : eArray[i] ;
+            eArray[i] = (i === 0)? capitalize(eArray[i+1]).trim() : eArray[i+1].trim();
+            eArray[i+1] = A.trim();
+        }
+        return eArray.join(" ");
+    }).join(". " + randomYoda() + " ");
+    newArray += " " + randomYoda();
+    return newArray;
 };
